381357655e658d2a5bb0ca5067d73f98a9b2a1e3
Vansh Soni <vanshsoni16@gmail.com>
2025-01-28 22:43:41 +0530

    Update extensions.js

--- extensions.js
+++ extensions.js
@@ -1,24 +1,23 @@
 const SVG_Thumb = `<svg width="24px" height="24px" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M5.29398 20.4966C4.56534 20.4966 4 19.8827 4 19.1539V12.3847C4 11.6559 4.56534 11.042 5.29398 11.042H8.12364L10.8534 4.92738C10.9558 4.69809 11.1677 4.54023 11.4114 4.50434L11.5175 4.49658C12.3273 4.49658 13.0978 4.85402 13.6571 5.48039C14.2015 6.09009 14.5034 6.90649 14.5034 7.7535L14.5027 8.92295L18.1434 8.92346C18.6445 8.92346 19.1173 9.13931 19.4618 9.51188L19.5612 9.62829C19.8955 10.0523 20.0479 10.6054 19.9868 11.1531L19.1398 18.742C19.0297 19.7286 18.2529 20.4966 17.2964 20.4966H8.69422H5.29398ZM11.9545 6.02658L9.41727 11.7111L9.42149 11.7693L9.42091 19.042H17.2964C17.4587 19.042 17.6222 18.8982 17.6784 18.6701L17.6942 18.5807L18.5412 10.9918C18.5604 10.8194 18.5134 10.6486 18.4189 10.5287C18.3398 10.4284 18.2401 10.378 18.1434 10.378H13.7761C13.3745 10.378 13.0488 10.0524 13.0488 9.65073V7.7535C13.0488 7.2587 12.8749 6.78825 12.5721 6.44915C12.4281 6.28794 12.2615 6.16343 12.0824 6.07923L11.9545 6.02658ZM7.96636 12.4966H5.45455V19.042H7.96636V12.4966Z" fill="white"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M5.29398 20.4966C4.56534 20.4966 4 19.8827 4 19.1539V12.3847C4 11.6559 4.56534 11.042 5.29398 11.042H8.12364L10.8534 4.92738C10.9558 4.69809 11.1677 4.54023 11.4114 4.50434L11.5175 4.49658C12.3273 4.49658 13.0978 4.85402 13.6571 5.48039C14.2015 6.09009 14.5034 6.90649 14.5034 7.7535L14.5027 8.92295L18.1434 8.92346C18.6445 8.92346 19.1173 9.13931 19.4618 9.51188L19.5612 9.62829C19.8955 10.0523 20.0479 10.6054 19.9868 11.1531L19.1398 18.742C19.0297 19.7286 18.2529 20.4966 17.2964 20.4966H8.69422H5.29398ZM11.9545 6.02658L9.41727 11.7111L9.42149 11.7693L9.42091 19.042H17.2964C17.4587 19.042 17.6222 18.8982 17.6784 18.6701L17.6942 18.5807L18.5412 10.9918C18.5604 10.8194 18.5134 10.6486 18.4189 10.5287C18.3398 10.4284 18.2401 10.378 18.1434 10.378H13.7761C13.3745 10.378 13.0488 10.0524 13.0488 9.65073V7.7535C13.0488 7.2587 12.8749 6.78825 12.5721 6.44915C12.4281 6.28794 12.2615 6.16343 12.0824 6.07923L11.9545 6.02658ZM7.96636 12.4966H5.45455V19.042H7.96636V12.4966Z" fill="currentColor"></path></svg>`
 
-
 export const DateExtension = {
   name: 'Date',
   type: 'response',
   match: ({ trace }) =>
     trace.type === 'ext_date' || trace.payload?.name === 'ext_date',
   render: ({ trace, element }) => {
-    const formContainer = document.createElement('form')
+    const formContainer = document.createElement('form');
 
     // Get current date and adjust for min and max date range
-    let currentDate = new Date()
-    let minDate = new Date()
-    minDate.setDate(currentDate.getDate() + 14) // 14 days from today
-    let maxDate = new Date()
-    maxDate.setFullYear(currentDate.getFullYear() + 1) // 1 year from today
+    let currentDate = new Date();
+    let minDate = new Date();
+    minDate.setDate(currentDate.getDate() + 14); // 14 days from today
+    let maxDate = new Date();
+    maxDate.setFullYear(currentDate.getFullYear() + 1); // 1 year from today
 
     // Convert to ISO string and remove time
-    let minDateString = minDate.toISOString().slice(0, 10)
-    let maxDateString = maxDate.toISOString().slice(0, 10)
+    let minDateString = minDate.toISOString().slice(0, 10);
+    let maxDateString = maxDate.toISOString().slice(0, 10);
 
     formContainer.innerHTML = `
           <style>
@@ -70,34 +69,47 @@ export const DateExtension = {
               opacity: 1; /* Make the button fully opaque when it's enabled */
             }
           </style>
-          <label for="date">Select your date</label><br>
-          <div class="meeting"><input type="date" id="meeting" name="meeting" value="" min="${minDateString}" max="${maxDateString}" /></div><br>
+          <label for="start-date">Select your start date</label><br>
+          <div class="meeting"><input type="date" id="start-date" name="start-date" value="" min="${minDateString}" max="${maxDateString}" /></div><br>
+          <label for="end-date">Select your end date</label><br>
+          <div class="meeting"><input type="date" id="end-date" name="end-date" value="" min="${minDateString}" max="${maxDateString}" /></div><br>
           <input type="submit" id="submit" class="submit" value="Submit" disabled="disabled">
-          `
+          `;
 
-    const submitButton = formContainer.querySelector('#submit')
-    const dateInput = formContainer.querySelector('#meeting')
+    const submitButton = formContainer.querySelector('#submit');
+    const startDateInput = formContainer.querySelector('#start-date');
+    const endDateInput = formContainer.querySelector('#end-date');
 
-    dateInput.addEventListener('input', function () {
-      if (this.value) {
-        submitButton.disabled = false
+    const validateInputs = () => {
+      if (startDateInput.value && endDateInput.value) {
+        const startDate = new Date(startDateInput.value);
+        const endDate = new Date(endDateInput.value);
+        submitButton.disabled = startDate <= endDate ? false : true;
       } else {
-        submitButton.disabled = true
+        submitButton.disabled = true;
       }
-    })
+    };
+
+    startDateInput.addEventListener('input', validateInputs);
+    endDateInput.addEventListener('input', validateInputs);
+
     formContainer.addEventListener('submit', function (event) {
-      event.preventDefault()
+      event.preventDefault();
 
-      const date = dateInput.value
-      console.log(date)
+      const startDate = startDateInput.value;
+      const endDate = endDateInput.value;
 
-      formContainer.querySelector('.submit').remove()
+      console.log({ startDate, endDate });
+
+      formContainer.querySelector('.submit').remove();
 
       window.voiceflow.chat.interact({
         type: 'complete',
-        payload: { date: date },
-      })
-    })
-    element.appendChild(formContainer)
+        payload: { startDate: startDate, endDate: endDate },
+      });
+    });
+
+    element.appendChild(formContainer);
   },
-}
+};
+
